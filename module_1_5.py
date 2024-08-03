832706f03f95f94d110ea1842d74b92f6547de23
viktor196011 <bridun.viktor@yandex.ru>
2024-08-03 08:28:26 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+# module_1_5.py
+immutable_var_ = [1,2,3.5, True,'real']
+immutable_var_1 = (1,2,3,4)
+immutable_var_2 = tuple([1,2,3,4])
+print(immutable_var_)
+print(immutable_var_1)
+print(immutable_var_2)
