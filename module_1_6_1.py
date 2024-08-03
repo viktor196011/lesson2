b5eaf7b606c974ae7f9fd294defd983b6fbd95ea
viktor196011 <bridun.viktor@yandex.ru>
2024-08-03 15:31:10 +0500

    Add files via upload

---
+++ module_1_6_1.py
@@ -0,0 +1,12 @@
+# module_1_6_1.py
+grades = [[5,3,3,5,4], [2,2,2,3], [4,5,5,2], [4,4,3], [5,5,5,4,5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+grades_m = sum(grades[0])/len(grades[0])
+print(grades_m)
+grades_m = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]),
+            sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])]
+print(grades_m)
+students_sort = sorted(students)
+print(students_sort)
+dict1 = dict(zip(students_sort,grades_m))
+print(dict1)
